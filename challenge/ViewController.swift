c1caa4d66b45bb644f6baaca8eea422184293664
Ezekiel Nieves <eznokoYT@outlook.com>
2019-11-13 08:51:00 -0600

    added label 1

---
+++ challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  challenge
+//
+//  Created by A.M. Student on 11/13/19.
+//  Copyright © 2019 A.M. Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
